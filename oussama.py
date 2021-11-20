1b6d73824989ffdaed21335bce3291afff337f6d
Oussalvatore <oussama.bekkouche@aalto.fi>
2021-11-20 13:44:34 +0200

    norm data

--- oussama.py
+++ oussama.py
@@ -24,42 +24,67 @@ def load_postal_code(file_path):
 
 def address_to_coordinates(address):
 
-    r = requests.get('https://api.digitransit.fi/geocoding/v1/search?text='+address+'&layers=address')
-    if r.status_code == 200:
-        response = r.json()
-        lon = response["features"][0]["geometry"]["coordinates"][0]
-        lat = response["features"][0]["geometry"]["coordinates"][1]
-        return lon, lat
-    else:
+    try:
+        r = requests.get('https://api.digitransit.fi/geocoding/v1/search?text='+address+'&layers=address')
+        if r.status_code == 200:
+            response = r.json()
+            lon = response["features"][0]["geometry"]["coordinates"][0]
+            lat = response["features"][0]["geometry"]["coordinates"][1]
+            return lon, lat
+        else:
+            return None
+    except:
         return None
 
-def normalize(x_list, x_min, x_max):
+def normalize(dataset_path):
 
-    to_return = []
-    for x in x_list:
-        to_return.append((x - x_min)/(x_max - x_min))
+    data = pd.read_csv(dataset_path)
+    df = pd.DataFrame(data,
+                      columns=['E'])
+
+    min = float('inf')
+    max = float('-inf')
+
+    for index, row in df.iterrows():
+
+        if row['E'] < min:
+            min = row['E']
+
+        if row['E'] > max:
+            max = row['E']
+
+    print(min, max)
+
+    for index, row in df.iterrows():
+        x = (row['E'] - min) / (max - min)
+        data.at[index, 'E'] = x
+
+    data.to_csv("data_normalized.csv")
+
 
 def build_data_set(mobility_ds, postal_codes):
 
-    with open("/Users/oussama/Downloads/5G/5G_tahtiluokka_1.json") as f:
+    with open("/home/mosaic/5G/5G_tahtiluokka_1.json") as f:
         five_g_1 = json.load(f)
 
-    with open("/Users/oussama/Downloads/5G/5G_tahtiluokka_2.json") as f:
+    with open("/home/mosaic/5G/5G_tahtiluokka_2.json") as f:
         five_g_2 = json.load(f)
 
-    with open("/Users/oussama/Downloads/5G/5G_tahtiluokka_3.json") as f:
+    with open("/home/mosaic/5G/5G_tahtiluokka_3.json") as f:
         five_g_3 = json.load(f)
 
     cache = {}
     data_set = []
     data = pd.read_csv(mobility_ds, sep=";")
     df = pd.DataFrame(data, columns=['DATE', 'LKM', 'KOTIPOSTINRO', 'POSTINRO'])
-    df = df.head(50)
+    df = df.head(1000)
     error_count = 0
     for index, row in df.iterrows():
         source_pc = str(row['KOTIPOSTINRO'])
         destination_pc = str(row['POSTINRO'])
         date = row['DATE']
+        day = to_day_of_week(date)
+
         lkm = row['LKM']
 
         if len(source_pc) < 5:
@@ -88,29 +113,34 @@ def build_data_set(mobility_ds, postal_codes):
                 lon_d, lat_d = cache[destination_address]
             else:
                 lon_d, lat_d = address_to_coordinates(destination_address)
-                cache[destination_address]  = lon_d, lat_d
+                cache[destination_address] = lon_d, lat_d
 
-            count_s = count_couvrage(five_g_1, lon_s, lat_s)
-            count_s += count_couvrage(five_g_2, lon_s, lat_s)
-            count_s += count_couvrage(five_g_3, lon_s, lat_s)
-            e_s = count_s/lkm
+            if (lon_s, lat_s) is not None:
 
-            count_d = count_couvrage(five_g_1, lon_d, lat_d)
-            count_d += count_couvrage(five_g_2, lon_d, lat_d)
-            count_d += count_couvrage(five_g_3, lon_d, lat_d)
-            e_d = count_d/lkm
-            
-            day = to_day_of_week(date)
+                count_s = count_couvrage(five_g_1, lon_s, lat_s)
+                count_s += count_couvrage(five_g_2, lon_s, lat_s)
+                count_s += count_couvrage(five_g_3, lon_s, lat_s)
+                e_s = count_s/lkm
+                data_set.append([lon_s, lat_s, day, e_s])
 
-            data_set.append([lon_s, lat_s, day, e_s])
-            data_set.append([lon_d, lat_d, day, e_d])
+            if (lon_d, lat_d) is not None:
+
+                count_d = count_couvrage(five_g_1, lon_d, lat_d)
+                count_d += count_couvrage(five_g_2, lon_d, lat_d)
+                count_d += count_couvrage(five_g_3, lon_d, lat_d)
+                e_d = count_d/lkm
+                data_set.append([lon_d, lat_d, day, e_d])
+
+
+        if (index % 100) == 0:
+            print(index)
 
     dsf = pd.DataFrame(data_set, columns=['Lon', 'Lat', 'Day', 'E'])
-
     dsf.to_csv("data.csv")
 
             # print(postal_codes[source_pc], postal_codes[destination_pc])
 
 if __name__ == "__main__":
-    postal_codes = load_postal_code("BAF_20211113.dat")
-    build_data_set("/Users/oussama/Downloads/elisa_liikkuvuus_dataset.csv", postal_codes)
+    #postal_codes = load_postal_code("BAF_20211113.dat")
+    #build_data_set("/home/mosaic/elisa_liikkuvuus_dataset.csv", postal_codes)
+    normalize("data.csv")
